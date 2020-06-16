ff5cc6586306385ea3a18c04b54cdf86f25f5150
Tomass <blanks.tomass@yahoo.lv>
2020-06-16 11:48:44 +0300

    created console project

---
+++ cvbot/cvbot.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace cvbot.console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
