4daa74d6e23f4ce80a36341dfd917fdb7fe7bf5f
Tomass <blanks.tomass@yahoo.lv>
2020-06-18 15:34:26 +0300

    bot goes through all ads and sends cv if not sent allready

--- cvbot/cvbot.Browser/CvBotChromeDriver.cs
+++ cvbot/cvbot.Browser/CvBotChromeDriver.cs
@@ -14,6 +14,9 @@ namespace cvbot.Browser
     {
         private IWebDriver driver;
         private string applicationText = "Labdien! Vēlos pieteikties vakancei.2019.gadā izgāju Codelex kursus un pašlaik paralēli mācos tālmācībā par programmētāju.2019.gada beigās sāku strādāt kā programmētājs, diemžēl covid-19 dēl tiku atlaists.Tagad esmu jauna darba meklējumos.Mans GitHub - https://github.com/TomB-MCMXCII";
+        private string english = "You have already applied for this position!Save";
+        private string latvian = "Jūs jau esat pieteicies šai vakancei!Saglabāt";
+
         public void Launch()
         {
             driver = new ChromeDriver();
@@ -39,12 +42,22 @@ namespace cvbot.Browser
                 toclick.FindElement(By.TagName("a")).Click();
                 var windowHandles = driver.WindowHandles;
                 driver.SwitchTo().Window(windowHandles[1]);
-                driver.FindElement(By.XPath("//*[@id=\"savebuttons\"]/button[1]")).Click();
-                driver.FindElement(By.XPath("//*[@id=\"userapplication\"]")).SendKeys(applicationText);
-                driver.FindElement(By.XPath("//*[@id=\"cvselect\"]")).FindElement(By.XPath("//*[@id=\"cvselect\"]/option[2]")).Click();
-                driver.FindElement(By.XPath("//*[@id=\"applyform\"]/form/div[8]/input")).Click();
-                driver.Close();
-                driver.SwitchTo().Window(windowHandles[0]);
+                var gg = driver.FindElement(By.XPath("//*[@id=\"savebuttons\"]")).Text;
+
+                if (driver.FindElement(By.XPath("//*[@id=\"savebuttons\"]")).Text == english || driver.FindElement(By.XPath("//*[@id=\"savebuttons\"]")).Text == latvian)
+                {
+                    driver.Close();
+                    driver.SwitchTo().Window(windowHandles[0]);
+                }
+                else
+                {
+                    driver.FindElement(By.XPath("//*[@id=\"savebuttons\"]/button[1]")).Click();
+                    driver.FindElement(By.XPath("//*[@id=\"userapplication\"]")).SendKeys(applicationText);
+                    driver.FindElement(By.XPath("//*[@id=\"cvselect\"]")).FindElement(By.XPath("//*[@id=\"cvselect\"]/option[2]")).Click();
+                    driver.FindElement(By.XPath("//*[@id=\"applyform\"]/form/div[8]/input")).Click();
+                    driver.Close();
+                    driver.SwitchTo().Window(windowHandles[0]);
+                }
             }
         }
 
