35b6a2e1eba8dea876f44c2e1f1576fa99f8273a
Tomass <blanks.tomass@yahoo.lv>
2020-06-18 12:41:31 +0300

    site login done.facebook login done

--- cvbot/cvbot.Browser/CvBotChromeDriver.cs
+++ cvbot/cvbot.Browser/CvBotChromeDriver.cs
@@ -6,6 +6,7 @@ using OpenQA.Selenium.Support.UI;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Threading;
 
 namespace cvbot.Browser
 {
@@ -13,23 +14,47 @@ namespace cvbot.Browser
     {
         private IWebDriver driver;
 
-       
-
         public void Launch()
         {
             driver = new ChromeDriver();
             
-            driver.Navigate().GoToUrl("https://www.cv.lv/darba-sludinajumi/informacijas-tehnologijas?sort=inserted&dir=desc");
+            driver.Navigate().GoToUrl("https://www.cv.lv");
+            driver.Manage().Window.Maximize();
+
             driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
-            var advertisements = driver.FindElements(By.ClassName("cvo_module_offer"));
-            ParseAdvertisements(advertisements);
+            Login();
+            FaceBookLogin();
+            Thread.Sleep(5000);
+
+            driver.Navigate().GoToUrl("https://www.cv.lv/darba-sludinajumi/informacijas-tehnologijas?sort=inserted&dir=desc");
+
+            var ads = driver.FindElements(By.ClassName("cvo_module_offer"));
+            ClickAdvertisements(ads);
         }
-        public void (ReadOnlyCollection<IWebElement> webElements)
+        public void ClickAdvertisements(ReadOnlyCollection<IWebElement> ads)
         {
-           
-                var toclick = webElements[5].FindElement(By.ClassName("offer_primary_info"));
+            foreach (var ad in ads)
+            {
+                var toclick = ad.FindElement(By.ClassName("offer_primary_info"));
                 toclick.FindElement(By.TagName("a")).Click();
-            
+
+            }
         }
+
+        public void Login()
+        {
+            var loginButton = driver.FindElement(By.ClassName("users-login"));
+            loginButton.FindElement(By.TagName("a")).Click();
+            var socialLogins = driver.FindElement(By.Id("social"));
+            socialLogins.FindElement(By.TagName("li")).Click();
+        }
+
+        public void FaceBookLogin()
+        {
+            driver.FindElement(By.Id("email")).SendKeys("25709976");
+            driver.FindElement(By.Id("pass")).SendKeys("TBlanks25071992");
+            driver.FindElement(By.Id("loginbutton")).Click();
+        }
+
     }
 }
